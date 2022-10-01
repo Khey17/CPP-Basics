383dcd752ec32175385186756c3f869a664d1a08
Karthikheyaa Kurra <90310297+Khey17@users.noreply.github.com>
2022-10-01 17:51:10 +0530

    Add files via upload

---
+++ Q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << INT_MAX << endl;
+    cout << INT_MIN << endl;
+    cout << UINT_MAX << endl;
+    cout << LLONG_MAX << endl;
+    cout << LLONG_MIN << endl;
+    cout << ULLONG_MAX << endl;
+    cout << CHAR_MAX << endl;
+    cout << CHAR_MIN << endl;
+    cout << SCHAR_MAX << endl;
+    cout << SCHAR_MIN << endl;
+    cout << UCHAR_MAX << endl;
+    cout << SHRT_MIN << endl;
+    cout << SHRT_MAX << endl;
+    cout << USHRT_MAX << endl;
+    
+    return 0;
+}
