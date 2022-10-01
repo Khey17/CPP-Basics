b0302a2b9e6129f5f80a3c8ef43537544bdec99f
Karthikheyaa Kurra <90310297+Khey17@users.noreply.github.com>
2022-10-01 17:57:40 +0530

    Add files via upload

---
+++ Char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;   // use only what you need
+using std::cin;     
+using std::endl;
+
+int main ()
+{
+    cout << "Enter your Initial: ";
+    
+    char First_Initial;
+    cin >> First_Initial;
+    
+    cout << "My First Initial is "<< First_Initial << "." << endl;
+    return 0;
+}
