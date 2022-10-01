37388bb63be7ffd28cf3d72b6edba8ab7f6bd785
Karthikheyaa Kurra <90310297+Khey17@users.noreply.github.com>
2022-10-01 17:59:08 +0530

    Add files via upload

---
+++ PrimitiveTypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;   // use only what you need
+using std::cin;     
+using std::endl;
+
+int main ()
+{
+   // cout << "Enter the Distance b/w Sun and Earth: ";
+    
+    long long int Dist (9200000000);
+    cout << "The Distance b/w Sun and Earth is " << Dist << " Miles." << endl;
+    
+    return 0;
+}
